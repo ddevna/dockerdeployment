6a48d53557ba8ac78989916172784ed74ea11ad0
D.D <deepakdevna@gmail.com>
2022-09-23 19:02:30 +0530

    chalja bhai

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return "Flask app is running like usain bolt"
+    return "Flask app is running pretty good"
 
 
 if __name__ == "__main__":
